5837a9624cd7d1696ab31b6c7dd7970ed0132a48
Alex Geer <geer.alex@gmail.com>
2020-12-05 19:04:00 -0500

    activated status update

--- src/bot.py
+++ src/bot.py
@@ -7,7 +7,7 @@ api = create_api()
 name = getName()
 
 try:
-    # api.update_status(name)
+    api.update_status(name)
     logger.info(name)
 except Exception as e:
     logger.error("Error status update", exc_info=True)
